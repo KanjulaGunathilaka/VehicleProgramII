7d913ba1984886d7fcc5765b93fc530106adba96
Kanjula Gunathilaka <ksgunathilaka071@gmail.com>
2024-05-14 17:51:21 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Security;
+using VehicleProgramII;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create instances of Vehicle class
+            Vehicle vehicle1 = new Vehicle();
+            Vehicle vehicle2 = new Vehicle("Kanjula Gunathilaka", 50, "left");
+
+            // Print field values of vehicle1
+            Console.WriteLine("Vehicle 1:");
+            Console.WriteLine($"ID: {vehicle1.Id}");
+            Console.WriteLine($"Owner Name: {vehicle1.OwnerName}");
+            Console.WriteLine($"Current Speed: {vehicle1.CurrentSpeed}");
+            Console.WriteLine($"Current Direction: {vehicle1.CurrentDirection}");
+
+            Console.WriteLine();
+
+            // Print field values of vehicle2
+            Console.WriteLine("Vehicle 2:");
+            Console.WriteLine($"ID: {vehicle2.Id}");
+            Console.WriteLine($"Owner Name: {vehicle2.OwnerName}");
+            Console.WriteLine($"Current Speed: {vehicle2.CurrentSpeed}");
+            Console.WriteLine($"Current Direction: {vehicle2.CurrentDirection}");
+
+            Console.ReadKey();
+        }
+    }
+
+
+
---
+++ vehicle.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleProgramII
+{
+    internal class Vehicle
+    {
+        // Fields
+        private string ownerName;
+        private double currentSpeed;
+        private string currentDirection;
+        private int id;
+
+        // Static fields
+        private static int vehicleId = 1;
+        private const int maxSpeed = 100;
+
+        // Properties
+        public string OwnerName { get { return ownerName; } }
+        public double CurrentSpeed { get { return currentSpeed; } }
+        public string CurrentDirection { get { return currentDirection; } }
+        public int Id { get { return id; } }
+
+        // Constructors
+        public Vehicle()
+        {
+            id = vehicleId++;
+            currentSpeed = 0;
+            currentDirection = "Forward";
+            ownerName = "Unknown";
+        }
+
+        public Vehicle(string ownerName, double currentSpeed, string currentDirection) : this()
+        {
+            this.ownerName = ownerName;
+            this.currentSpeed = currentSpeed;
+            this.currentDirection = currentDirection;
+        }
+
+        // Methods
+        public void ChangeSpeed(double newSpeed)
+        {
+            if (newSpeed >= 0 && newSpeed <= maxSpeed)
+            {
+                currentSpeed = newSpeed;
+            }
+            else
+            {
+                Console.WriteLine("Invalid speed value.");
+            }
+        }
+
+        public void Stop()
+        {
+            currentSpeed = 0;
+        }
+    }
+}
